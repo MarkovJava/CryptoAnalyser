3ef7ff5361581c393bfb1de8d62877bc6c0a6e9a
MarkovJava <markov.java@gmail.com>
2022-07-19 12:56:02 +0300

    Comments

--- src/main/java/ru/javarush/cryptoanaliser/markov/application/Application.java
+++ src/main/java/ru/javarush/cryptoanaliser/markov/application/Application.java
@@ -8,13 +8,17 @@ import ru.javarush.cryptoanaliser.markov.application.view.commandLine.CommandLin
 public class Application {
 
     /*
-    0 - комманда
+    0 - команда
     1 - путь до исходного файла
     3 - ключ
 
+    Какие ошибки заметил сам:
 
-
+    1. Текстовый файл с результатом один, вшит. Исправить.
+    2. Исключения не обрабатываются должным образом.
+    3. Какие-то участки "просят" комментарии. Рефакторинг.
      */
+
     public void execute(String[] args) {
         Controller controller = new MainController(new FileSystem(), new CommandLine(args));
         controller.execute();
